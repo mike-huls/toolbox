923c3bbb84e3e9cb77e826ef4ae8588f1f9c249a
unknown <m.huls@datanext.nl>
2021-06-21 11:10:48 +0200

    initial upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+# Ensures that we can install a package with pip install package
+
+from setuptools import setup
+
+setup(
+    name='ppg',
+    version='0.1',
+    description='Testing installation of Package',
+    # py_modules=["ppack"],
+    # package_dir={'': '.'},
+    url='#',
+    author='Mike Huls',
+    author_email='mike_huls@hotmail.com',
+    license='MIT',
+    packages=['ppg'], # same as name
+    zip_safe=False,
+    install_requires=['wheel', 'requests'],  # external packages as dependencies
+)
