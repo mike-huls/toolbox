3cc7368014d8f268c689b583cf688436287e48b9
unknown <m.huls@datanext.nl>
2021-06-21 11:16:45 +0200

    test

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# This is so that you can import ppack or
-#   import average from ppack       in stead of from ppack.functions import average
-
-from ppg.src.functions import power, test
---
+++ ppg/__init__.py
@@ -0,0 +1,3 @@
+# This is so that you can import ppack or import average from ppack
+# in stead of from ppack.functions import average
+
