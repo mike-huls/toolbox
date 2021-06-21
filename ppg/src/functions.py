79edccd9f71b61d6cf9ca8dc71e58fea358a3884
unknown <m.huls@datanext.nl>
2021-06-21 11:12:28 +0200

    added src

---
+++ ppg/src/functions.py
@@ -0,0 +1,11 @@
+def sum(x,y):
+    return x+y
+
+def average(x,y):
+    return (x+y)/2
+
+def power(x,y):
+    return x**y
+
+def test():
+    print('ppack works')
---
+++ ppg/src/greet.py
@@ -0,0 +1,2 @@
+def SayHello(name):
+    print("Hello ", name)
