800f220559be21c84dd7b6d95963c32733834098
unknown <m.huls@datanext.nl>
2021-06-25 17:08:56 +0200

    corrected version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='toolbox',
-    version='0.2',
+    version='0.0.1',
     author='Mike Huls',
     author_email='mike_huls@hotmail.com',
 
